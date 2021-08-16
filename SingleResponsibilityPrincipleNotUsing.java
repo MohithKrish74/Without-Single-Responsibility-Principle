6fea97ccbb47287305e1093d47244c874e29e15f
MohithKrish74 <87643124+MohithKrish74@users.noreply.github.com>
2021-08-16 17:16:51 +0530

    Update SingleResponsibilityPrincipleNotUsing.java

--- SingleResponsibilityPrincipleNotUsing.java
+++ SingleResponsibilityPrincipleNotUsing.java
@@ -1,46 +1,18 @@
 package com.keyword;
 
-class Student       // A class containing multiple functions.This violates Single Responsibility Principle.
+class SalesOrder // A class containing multiple functions.This violates Single Responsibility Principle.
 {
-    public void studentId()
+    public void salesOrder()
     {
-        System.out.println("Id : 1");
+        System.out.println("Sales Order.");
     }
-    public void studentName()
+    public void saveOrder()
     {
-        System.out.println("Name : Ram");
+        System.out.println("Save Order.");
     }
-    public void studentAge()
+    public void export()
     {
-        System.out.println("Age : 18");
-    }
-    public void studentGender()
-    {
-        System.out.println("Gender : Male");
-    }
-    public void studentStandard()
-    {
-        System.out.println("Student Standard : 10th ");
-    }
-    public void schoolName()
-    {
-        System.out.println("School Name : Montessori");
-    }
-    public void studentRank()
-    {
-        System.out.println("Student Rank : 2");
-    }
-    public void studentTotalMark()
-    {
-        System.out.println("Student Mark : 490/500");
-    }
-    public void studentDepartment()
-    {
-        System.out.println("Student Department : Computer Science");
-    }
-    public void schoolFee()
-    {
-        System.out.println("School Fee : 30,000");
+        System.out.println("Export the Order.");
     }
 }
 
@@ -48,17 +20,9 @@ public class SingleResponsibilityPrincipleNotUsing
 {
     public static void main(String[] args)
     {
-        Student obj = new Student();
-        System.out.println("The Details are : ");
-        obj.studentId();
-        obj.studentName();
-        obj.studentAge();
-        obj.studentGender();
-        obj.studentStandard();
-        obj.schoolName();
-        obj.studentRank();
-        obj.studentTotalMark();
-        obj.studentDepartment();
-        obj.schoolFee();
+        SalesOrder salesOrder = new SalesOrder();
+        salesOrder.salesOrder();
+        salesOrder.saveOrder();
+        salesOrder.export();
     }
 }
